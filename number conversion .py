185fa9cc207754301431954c564a63e4dbce9118
PAVAN VENKATESH KONETI <pavankoneti85@gmail.com>
2026-06-12 15:28:25 +0530

    Add files via upload

---
+++ number conversion .py
@@ -0,0 +1,47 @@
+'''binary to decimal conversion'''
+# a=0b10111
+# print (a)
+
+# a=0b11001
+# print (a)
+
+# a=0b111111
+# print (a)
+
+# a=0b100101
+# print (a)
+
+# a=0b1011010
+# print (a)
+
+'''octal to decimal conversion'''
+# a=0o17
+# print (a)
+
+# a=0o25
+# print (a)
+
+# a=0o76
+# print (a)
+
+# a=0o123
+# print (a)
+
+# a=0o345
+# print (a)
+
+'''hexadecimal to decimal conversion'''
+# a=0x1B
+# print (a)
+
+# a=0x2F
+# print (a)
+
+# a=0xAC
+# print (a)
+
+# a=0x1D4
+# print (a)
+
+# a=0x3E8
+# print (a)
